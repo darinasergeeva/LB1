3780f64e1f2e162382825d3b4d23af4f3247f87e
Дарина Сергеева <darinasergeeva122@gmail.com>
2024-11-21 16:43:10 +0500

    Добавление связи с БД, используя метод EnsureCreated, вывод списка пользователей

---
+++ LB1/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LB1
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null;
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=1111");
+        }
+    }
+
+}
+
